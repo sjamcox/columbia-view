240ae3466afc071c5600dc84e193d81cb3097c4d
Sam Cox <sjamcox@gmail.com>
2021-05-30 17:44:51 -0700

    Fix page error in schema

--- studio/schemas/objects/internalLink.js
+++ studio/schemas/objects/internalLink.js
@@ -8,7 +8,7 @@ export default {
       type: 'reference',
       title: 'Reference',
       to: [
-        { type: 'page' },
+        { type: 'landingPage' },
         { type: 'series' },
         { type: 'sermon' },
       ]
