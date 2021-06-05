f828c42af3b0bba494035614f457466e987aaf5f
Danny Harmon <dannyaharmon@gmail.com>
2021-06-04 20:56:19 -0700

    working progress

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
-import styles from './navbar.css'
+import React, { useState } from 'react'
+import styles from './navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Navbar = ({ links }) => (
+const Navbar = ({ links }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const openMenu = () => {
+    setShowMenu(true)
+  }
+  const closeMenu = () => {
+    setShowMenu(false)
+    document.removeEventListener('click', closeMenu)
+  }
+
+  const linkTags = links.map((link,i) => (
+    <Link key={i} className={styles.link} href={`/${link}`}>
+      <p className={styles.p}>{link}</p>
+    </Link>
+  ))
+  return (
   <div className={styles.nav}>
 
     <div className="LogoContainter">
-      <img src="Logo"></img>
-      <img src="NameLogo"></img>
+      <Image className={styles.img} src="/logo.png" width="50px" height="50px" alt="logo"/>
+      <Image className={styles.img} src="/logo.png" width="50px" height="50px" alt="logoName" />
     </div>
 
-    <div className="LinksContainer">
-      {links.map(link => {
-        <Link href={link}>
-          <a>{link}</a>
-        </Link>
-      })}
+    <div className={styles.linkContainer}>
+      {linkTags}
     </div>
 
-    <Image id="HamMenu" src="/HamburgerMenu" alt="HamMenu"></img>
+      <div
+        className={styles.hamMenu}
+        onClick={openMenu}
+      >
+        <Image
+          src="/hamMenu.png"
+          alt="HamMenu"
+          width="50px"
+          height="50px"
+        />
+        {showMenu && (
+          <div className={styles.menu}>
+            {linkTags}
+          </div>
+        )}
+      </div>
+
   </div>
-)
 
+)
+}
 
 export default Navbar
