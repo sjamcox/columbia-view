1d2c11027aa9fba5bf30d95419ec670a4e5aa374
Sam Cox <sjamcox@gmail.com>
2021-06-12 00:19:19 -0700

    Add queries for MainNav and SocialBlock

--- client/client.js
+++ client/client.js
@@ -1,7 +1,26 @@
 import sanityClient from '@sanity/client'
 
-export default sanityClient({
+export const client = sanityClient({
   projectId: '9ya152ax',
   dataset: 'production',
   useCdn: true // `false` if you want to ensure fresh data
 })
+
+export const getMainNavigationItems = () => {
+  const query = `
+    *[_id == 'mainNavigation'][0] {
+      items[] {
+        'link': reference->{title, 'slug': slug.current},
+        children[] {
+          'link': reference->{title, 'slug': slug.current}
+        }
+      }
+    }
+  `
+  return client.fetch(query)
+}
+
+export const getSocialAccounts = () => {
+  const query = `*[_id == 'socialAccounts'][0]`
+  return client.fetch(query)
+}
