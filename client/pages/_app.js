3797ba11296d1e26db3ef247fb181a87dd65db50
Sam Cox <sjamcox@gmail.com>
2021-06-12 00:20:15 -0700

    Custom App with getInitialProps and queries

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,1 +1,23 @@
-//Global styles
+import App from 'next/app'
+import { getMainNavigationItems, getSocialAccounts } from '../client'
+import { NavBar } from '../components/NavBar/NavBar'
+import { SocialBlock } from '../components/SocialBlock/SocialBlock'
+
+const MyApp = ({ Component, pageProps, navProps, socialProps }) => {
+  return (
+    <>
+      <NavBar items={navProps.items}/>
+      <Component {...pageProps} />
+      <SocialBlock links={socialProps} />
+    </>
+  )
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext)
+  const navProps = await getMainNavigationItems()
+  const socialProps = await getSocialAccounts()
+  return { ...appProps, navProps, socialProps }
+}
+
+export default MyApp
