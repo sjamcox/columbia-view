f6c73aeb7c54d33408ec28f0058d85d96c7d9b04
Sam Cox <sjamcox@gmail.com>
2021-11-16 21:29:01 -0800

    Build all pages

---
+++ client/pages/give.js
@@ -0,0 +1,18 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  Link,
+} from '@mui/material'
+import { Layout } from '../components/Layout'
+
+export default function Give() {
+  return (
+    <Layout>
+      <Container></Container>
+    </Layout>
+  )
+}
---
+++ client/pages/ministries.js
@@ -0,0 +1,95 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  Link,
+} from '@mui/material'
+import { Layout } from '../components/Layout'
+
+export default function Ministries() {
+  return (
+    <Layout>
+      <Typography>Columbia View's Ministries</Typography>
+      <Typography>
+        Our ministries can be summed up in three words: belong, believe, and
+        become. First, our ministries provide both places and spaces where you
+        can belong just as you are. We are a diverse and eclectic group of
+        people all seeking to know God’s heart. Second, our ministries are
+        designed to help you move towards greater alignment with the Bible. We
+        believe the story of the Bible ultimately points to Jesus, and through
+        belief in Jesus, we can truly know God’s heart. Lastly, we view our
+        ministries as tools God can use to shape you to become the person he
+        intends you to be; reflecting the life and character of Jesus as you
+        seek to share his heart with your people.
+      </Typography>
+      <Typography>
+        Best Year's Fellowship Best Year's Fellowship (BYF) is a great place for
+        those "seasoned" to get connected, enjoy a meal, pray, have fun, and
+        grow together through learning about the Bible with one another. All are
+        welcome to attend BYF's once-a-month Life Group! ​ Date and Time Second
+        Tuesday of each month 11:00 am - 12:30 pm ​ Location Columbia View
+        Wesleyan Church in the Columbia Room
+      </Typography>
+      <Typography>
+        Children's Ministry We offer a children's class during our Sunday
+        morning preaching portion of our worship service. Our Children's
+        Ministry team are all background checked and trained to ensure the
+        highest quality and safety for your children. Children's Church 10:00 am
+      </Typography>
+      <Typography>
+        Global Missions Blake Family Czech Republic The Blake family has been
+        serving in Czech Republic since 2003. They are currently focused on
+        developing relationships, discipleship and church leadership. To learn
+        more or to donate, visit:
+        https://www.globalpartnersonline.org/missionary/wm04-0264/ Cheney
+        Family.jpg Cheney Family Swaziland The Cheney's have been missionaries
+        with Global Partners since 2010. They currently live in South Africa.
+        Recently, their focus is in developing and strengthening churches and
+        communities through theological education and health initiatives. To
+        learn more or to donate, visit:
+        https://www.globalpartnersonline.org/missionary/wm04-0368/ Dunbar.jpg
+        Mark and Serena Dunbar Mexico The Dunbar's have been missionaries with
+        World Gospel Missions for 25+ years. They currently serve alongside
+        Mexican church in the area of theological education, employment skills,
+        and discipleship. To learn more or to donate, visit:
+        https://www.wgm.org/missionary/dunbar Jones Family.jpg Jones Family
+        Wycliff Bible Translators Milt and Becky Jones have served with the Seed
+        Company working alongside Wycliffe Bible Translators. Recently, Milt has
+        been asked to support and oversee translations for the continents of
+        Africa and Asia. To learn more or to donate, visit:
+        https://www.wycliffe.org/partner/E7666D
+      </Typography>
+      <Typography>
+        Life groups are the heartbeat of our church as we seek to live out the
+        practice of Acts 2:42-47. We currently have Life Groups on Sundays,
+        Tuesdays, Wednesdays, and Thursdays. All are welcome and encouraged to
+        join a Life Group that best fits them regardless of Biblical experience.
+        Sundays Sermon and Worship service 10:00 am Children's Church 10:00 am ​
+        Tuesdays BYF (second Tuesday of the month) 11:00 am Location: Church
+        building "Banding Together" 2:00 pm Location: Powell & 23rd "Women of
+        the Bible " 7:00 pm Location: Currently meeting via Zoom ​ Wednesdays
+        Men's Banding Together Life Group 7:00 pm Location: Church building
+        "Banding Together" 7:30 pm Young Adult Group in the Parklane
+        Neighborhood
+      </Typography>
+      <Typography>
+        Worship Service Every Sunday, we gather and worship through music,
+        prayer, preaching/teaching of God's word, giving, and testimony. Worship
+        for us is the response of a life that is seeing after Jesus. If you are
+        a guest with us, we encourage you to come with an open heart to what God
+        may want to do in your life, and don't worry, we will not embarrass or
+        call on you during worship. Worship Service: 10:00 am ​ ​ ​ ​ We also
+        live stream our worship services via Facebook each Sunday at 10:00 am.
+      </Typography>
+      <Typography>
+        Youth Ministry We offer periodic outings and yearly retreats, camps, and
+        service opportunities. Rather than a traditional "youth group," we seek
+        to integrate youth into the overall life of our church through our
+        worship services and life groups.
+      </Typography>
+    </Layout>
+  )
+}
