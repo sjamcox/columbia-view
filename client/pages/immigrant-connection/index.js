4c80a536e670390b8b24fce4eab1ff0328944f14
Sam Cox <sjamcox@gmail.com>
2021-11-28 19:41:10 -0800

    Combine staff page into ICPDX page

--- client/pages/immigrant-connection/index.js
+++ client/pages/immigrant-connection/index.js
@@ -19,6 +19,12 @@ import residence from '../../public/residence.webp'
 import status from '../../public/status.webp'
 import visa from '../../public/visa.webp'
 import world from '../../public/world.webp'
+import tita from '../../public/staff/tita.webp'
+import brook from '../../public/staff/brook.webp'
+import david from '../../public/staff/david.webp'
+import susan from '../../public/staff/susan.webp'
+import vanessa from '../../public/staff/vanessa.webp'
+import niki from '../../public/staff/niki.webp'
 
 export default function ImmigrantConnection() {
   const [isOpen, setIsOpen] = useState(false)
@@ -237,6 +243,137 @@ export default function ImmigrantConnection() {
             </MuiLink>
           </Collapse>
         </Box>
+        <Typography variant="h4" paragraph textAlign="center" mt={6}>
+          <strong>Our Team</strong>
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={tita} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>Serena Rannabargar</strong>
+            </Typography>
+            <Typography fontSize={15}>DOJ Accredited Representative</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English, Spanish
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={brook} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>Brook Rodrigues</strong>
+            </Typography>
+            <Typography fontSize={15}>Legal Assistant</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={david} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>David Rannabargar</strong>
+            </Typography>
+            <Typography fontSize={15}>Site Director</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={susan} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>Susan Sloan</strong>
+            </Typography>
+            <Typography fontSize={15}>Treasurer</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={vanessa} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>Vanessa Cisneros</strong>
+            </Typography>
+            <Typography fontSize={15}>Media</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            mt={4}
+            xs={12}
+            sm={6}
+            md={4}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={niki} width="128px" height="128px" />
+            <Typography mt={3} color="primary">
+              <strong>Niki Cox</strong>
+            </Typography>
+            <Typography fontSize={15}>General Support</Typography>
+            <Typography fontSize={15} color="#A2230A">
+              Languages: English
+            </Typography>
+          </Grid>
+        </Grid>
       </Container>
     </Layout>
   )
