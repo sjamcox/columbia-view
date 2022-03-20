daf6b749e57fcf1e8e55084aaa62d5a2d83b79e4
Sam Cox <sjamcox@gmail.com>
2022-03-20 15:36:20 -0700

    Return HTML response

--- client/pages/api/deploy.js
+++ client/pages/api/deploy.js
@@ -1,15 +1,19 @@
 import axios from 'axios'
 
 export default async function handler(req, res) {
+  res.setHeader('Content-type', 'text/html')
   try {
     await axios.get(process.env.REDEPLOY_HOOK_URL)
-    res.status(200).json({
-      message:
-        'Redeploy successfully started. Should be complete in 2-5 minutes.',
-    })
+    res
+      .status(200)
+      .send(
+        '<h3>Redeploy successfully started. Should be complete in 2-5 minutes.</h3>'
+      )
   } catch (err) {
-    res.status(500).json({
-      message: 'Something went wrong. Try again or let your web admin know.',
-    })
+    res
+      .status(500)
+      .send(
+        '<h3>Something went wrong. Try again or let your web admin know.</h3>'
+      )
   }
 }
