ae5e31ca7238bb1fb6ecc5bb298c60c05d0186a7
Sam Cox <sjamcox@gmail.com>
2022-10-09 23:10:37 -0700

    use client-side fetching and useQuery for calendar

--- client/pages/events.js
+++ client/pages/events.js
@@ -2,39 +2,21 @@ import React from 'react'
 import { Container, Typography } from '@mui/material'
 import Head from 'next/head'
 import { Layout } from '../components/Layout'
-import { dehydrate, QueryClient, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 
 import getCalendar from '../queries/getCalendar'
 import CalendarList from '../components/CalendarList'
 
-export async function getServerSideProps() {
-  const queryClient = new QueryClient()
-  const date = new Date()
-  const startDate = date.toISOString()
-  date.setMonth(date.getMonth() + 3)
-  const endDate = date.toISOString()
-  const { data } = await getCalendar(startDate, endDate)
-  console.log(data)
-  // await queryClient.prefetchQuery(['calendar', startDate, endDate], () =>
-  //   getCalendar(startDate, endDate)
-  // )
-  return {
-    props: {
-      startDate,
-      endDate,
-      calendar: data,
-      // dehydratedState: dehydrate(queryClient),
-    },
-  }
-}
+const date = new Date()
+const startDate = date.toISOString()
+date.setMonth(date.getMonth() + 3)
+const endDate = date.toISOString()
 
-export default function Calendar({ calendar, startDate, endDate }) {
-  const queryClient = useQueryClient()
-  // const { data: calendar } = queryClient.getQueryData([
-  //   'calendar',
-  //   startDate,
-  //   endDate,
-  // ])
+export default function Calendar() {
+  const { data: calendar } = useQuery(['calendar', startDate, endDate], () =>
+    getCalendar(startDate, endDate)
+  )
+  if (!calendar) return <></>
   return (
     <Layout>
       <Head>
