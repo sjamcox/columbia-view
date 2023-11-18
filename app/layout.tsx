27acfb631fa7f6a07f68aeb61853ad850676572d
Sam Cox <sjamcox@gmail.com>
2023-11-18 09:48:34 -0800

    Add default metadata to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from 'next'
 import { Box } from '@mui/material'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Providers from '../providers'
+
+export const metadata: Metadata = {
+  title: 'Columbia View Church',
+  description:
+    'Columbia View Church is a Christ-centered church where the Word of God is shared and people are loved. Advancing the great commission in the spirit of the great commandment.',
+}
 
 export default function RootLayout({
   children,
