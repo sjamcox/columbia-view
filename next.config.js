ac7b1084b6f13e304fe15b75564a90961e5650dc
Sam Cox <sjamcox@gmail.com>
2024-10-08 17:46:16 -0700

    add redirect from /events to external calendar

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/events',
+        destination: 'https://columbiaview.churchcenter.com/calendar?view=list',
+        permanent: true,
+      },
+    ]
+  },
 }
