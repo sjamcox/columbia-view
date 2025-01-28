e358491dcc7ef38c403acf3bfed3e3258debe658
Sam Cox <sjamcox@gmail.com>
2025-01-27 20:17:17 -0800

    add more episode types

--- types.ts
+++ types.ts
@@ -1,3 +1,39 @@
+export type EpisodeSummary = {
+  episode_id: number
+  type: string
+  title: string
+  duration: number
+  show_id: number
+  author_id: number
+  site_url: string
+  image_url: string
+  image_original_url: string
+  published_at: string
+  download_enabled: boolean
+}
+
+export type EpisodeList = EpisodeSummary[]
+
+export type Author = {
+  user_id: number
+  fullname: string
+  site_url: string
+  image_url: string
+  image_original_url: string
+  accept_latest_tos_url: string | null
+}
+
+export type Show = {
+  show_id: number
+  title: string
+  image_url: string
+  image_original_url: string
+  explicit: boolean
+  author_id: number
+  last_episode_at: string
+  site_url: string
+}
+
 export type Episode = {
   episode_id: number
   type: string
@@ -9,7 +45,7 @@ export type Episode = {
   image_url: string
   image_original_url: string
   image_transformation: string
-  published_at: '2023-10-29 18:46:31'
+  published_at: string
   download_enabled: boolean
   stream_id: number | null
   waveform_url: string
@@ -18,36 +54,20 @@ export type Episode = {
   playback_url: string
   description: string
   description_html: string
-  permalink: 'exodus-16-wilderness'
-  episode_link: null
-  transcript_url: null
-  transcript_type: null
-  rss_guid: 'https://api.spreaker.com/episode/57434186'
-  plays_count: 0
-  downloads_count: 6
-  plays_ondemand_count: 0
-  plays_live_count: 0
-  likes_count: 0
-  messages_count: 0
-  chapters_count: 0
-  author: {
-    user_id: 10804617
-    fullname: 'Columbia View Church'
-    site_url: 'https://www.spreaker.com/user/columbiaview'
-    image_url: 'https://d1bm3dmew779uf.cloudfront.net/large/4061313ee6d1a05535b11e6670f213f2.jpg'
-    image_original_url: 'https://d3wo5wojvuv7l.cloudfront.net/images.spreaker.com/original/4061313ee6d1a05535b11e6670f213f2.jpg'
-    accept_latest_tos_url: null
-  }
-  show: {
-    show_id: 3172208
-    title: 'Columbia View Podcast'
-    image_url: 'https://d1bm3dmew779uf.cloudfront.net/large/4061313ee6d1a05535b11e6670f213f2.jpg'
-    image_original_url: 'https://d3wo5wojvuv7l.cloudfront.net/images.spreaker.com/original/4061313ee6d1a05535b11e6670f213f2.jpg'
-    explicit: false
-    author_id: 10804617
-    last_episode_at: '2023-10-29 18:46:31'
-    site_url: 'https://www.spreaker.com/show/sermons_59'
-  }
+  permalink: string
+  episode_link: string | null
+  transcript_url: string | null
+  transcript_type: string | null
+  rss_guid: string
+  plays_count: number
+  downloads_count: number
+  plays_ondemand_count: number
+  plays_live_count: number
+  likes_count: number
+  messages_count: number
+  chapters_count: number
+  author: Author
+  show: Show
   season_number: number
   episode_number: number
   season_episode_type: string
