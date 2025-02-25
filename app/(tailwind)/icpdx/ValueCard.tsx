a21016ac8211196471f3eaa7b6fc39d64a882718
Sam Cox <sjamcox@gmail.com>
2025-02-24 22:37:33 -0800

    desktop mission and values

---
+++ app/(tailwind)/icpdx/ValueCard.tsx
@@ -0,0 +1,26 @@
+type ValueCardProps = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export default function ValueCard({
+  title,
+  subtitle,
+  description,
+}: ValueCardProps) {
+  return (
+    <div className="flex gap-7">
+      <div className="from-primary-light-blue to-secondary-aqua h-30 w-30 shrink-0 rounded-full bg-gradient-to-b"></div>
+      <div className="rounded-2xl bg-white p-7">
+        <h3 className="font-display mb-4 text-2xl font-semibold">
+          {title}{' '}
+          <span className="text-neutral-mid-gray font-normal italic">
+            {subtitle}
+          </span>
+        </h3>
+        <p className="text-base">{description}</p>
+      </div>
+    </div>
+  )
+}
--- app/(tailwind)/icpdx/page.tsx
+++ app/(tailwind)/icpdx/page.tsx
@@ -3,7 +3,10 @@ import type { Metadata } from 'next'
 import { ImageSection } from 'components/Section'
 import immigrantSuccess from 'public/immigrant-success-stories.webp'
 import icpdxLogo from 'public/logos/icpdx-logo-white.svg'
+import icpdxStaff from 'public/icpdx/icpdx-staff-conf.jpeg'
 import Image from 'next/image'
+import Divider from 'components/Divider'
+import ValueCard from './ValueCard'
 
 export const metadata: Metadata = {
   title: 'Partner With Us | Immigrant Connection',
@@ -15,7 +18,7 @@ export default function ImmigrantConnection() {
   return (
     <main>
       <ImageSection src={immigrantSuccess} alt="Immigrant family celebrating">
-        <div className="mx-auto flex w-full max-w-6xl flex-col gap-10 px-4 py-32">
+        <div className="mx-auto flex w-full max-w-6xl flex-col gap-10 px-4 py-25">
           <Image
             src={icpdxLogo}
             alt="Immigrant Connection PDX logo"
@@ -27,6 +30,84 @@ export default function ImmigrantConnection() {
           </h1>
         </div>
       </ImageSection>
+      <Divider />
+      <section className="mx-auto grid w-full max-w-6xl grid-cols-12 flex-col gap-x-2 px-4 py-25">
+        <div className="relative col-start-2 col-end-7 overflow-hidden rounded-2xl">
+          <Image
+            src={icpdxStaff}
+            alt="Immigrant Connection staff at a conference"
+            width={768}
+            height={1024}
+          />
+        </div>
+        <div className="col-start-8 col-end-12 flex items-center">
+          <div>
+            <h2 className="text-primary-dark-blue font-display text-7xl/28 font-bold uppercase">
+              Vision
+            </h2>
+            <p className="mb-10">Building bridges for a hope-filled future.</p>
+            <h2 className="text-primary-dark-blue font-display text-7xl/28 font-bold uppercase">
+              Mission
+            </h2>
+            <p>
+              Providing low-cost immigration legal services to the
+              under-resourced communities of the Portland metro area and beyond.
+            </p>
+          </div>
+        </div>
+      </section>
+      <Divider />
+      <section className="bg-primary-dark-blue w-full">
+        <div className="mx-auto flex w-full max-w-6xl flex-col px-4 py-25">
+          <h2 className="font-display mb-10 self-center text-7xl/28 font-bold text-white uppercase">
+            Our Values
+          </h2>
+          <div className="grid grid-cols-12 grid-rows-3 gap-y-13">
+            <div className="col-start-2 col-end-10">
+              <ValueCard
+                title="Listening"
+                subtitle="with compassion"
+                description="Because the stories of immigrants are often filled with deep challenges and trauma, we listen with care, empathy, and understanding. We recognize the human side of every case, offering a safe space for each person’s story."
+              />
+            </div>
+            <div className="col-start-4 col-end-12">
+              <ValueCard
+                title="Advising"
+                subtitle="with expertise"
+                description="Because immigration laws are constantly evolving, we continuously learn and adapt to provide our clients with the most accurate, up-to-date pathways. Our expertise is a bridge to clarity and confidence in the journey ahead."
+              />
+            </div>
+            <div className="col-start-2 col-end-10">
+              <ValueCard
+                title="Speaking"
+                subtitle="the truth"
+                description="Because the immigration journey is complex and every step matters, we are committed to building trust by being honest and transparent with our clients. Accurate immigration filings are the foundation of safe and successful futures."
+              />
+            </div>
+            <div className="col-start-4 col-end-12">
+              <ValueCard
+                title="Empowering"
+                subtitle="through knowledge"
+                description="Because navigating the immigration process can be overwhelming, we empower our clients with the knowledge and resources they need to take an active role in their journey, making informed decisions along the way."
+              />
+            </div>
+            <div className="col-start-2 col-end-10">
+              <ValueCard
+                title="Walking"
+                subtitle="with you"
+                description="Because the immigration process can be long and uncertain, we stand beside our clients throughout the journey, staying engaged, informed, and ready to support them every step of the way."
+              />
+            </div>
+            <div className="col-start-4 col-end-12">
+              <ValueCard
+                title="Celebrating"
+                subtitle="the milestones"
+                description="Because the ultimate goal of immigration is stability, opportunity, and family reunification, we celebrate each step forward. Every milestone, big or small, brings us closer to the goal of building lasting futures."
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
