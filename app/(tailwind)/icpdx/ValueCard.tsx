672a0b091b44366ab035a8105f3e049147d42c91
Sam Cox <sjamcox@gmail.com>
2025-02-27 17:23:30 -0800

    update ValueCard

--- app/(tailwind)/icpdx/ValueCard.tsx
+++ app/(tailwind)/icpdx/ValueCard.tsx
@@ -10,7 +10,7 @@ export default function ValueCard({
   description,
 }: ValueCardProps) {
   return (
-    <div className="flex gap-7">
+    <div className="flex items-center gap-7">
       <div className="from-primary-light-blue to-secondary-aqua h-30 w-30 shrink-0 rounded-full bg-gradient-to-b"></div>
       <div className="rounded-2xl bg-white p-7">
         <h3 className="font-display mb-4 text-2xl font-semibold">
