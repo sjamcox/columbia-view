86d68f70b06fc0f29646edca8a078a0fed477bd0
Sam Cox <sjamcox@gmail.com>
2025-04-15 21:39:14 -0700

    finish KeywordCard component

--- app/(tailwind)/nextgen/page.tsx
+++ app/(tailwind)/nextgen/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 
 import Hero from 'components/Hero'
 import immigrantSuccess from 'public/immigrant-success-stories.webp'
+import KeywordCard from 'components/KeywordCard'
 
 export const metadata: Metadata = {
   title: 'NextGen Ministries | Columbia View Church',
@@ -23,7 +24,7 @@ export default function NextGen() {
           text: 'PRE-REGISTER',
         }}
       />
-      <section className="mx-auto w-full max-w-6xl flex-col px-6 py-15 lg:py-25">
+      <section className="mx-auto w-full max-w-6xl px-6 py-15 lg:py-25">
         <h2 className="font-display from-primary-light-blue to-secondary-aqua mb-15 bg-gradient-to-b bg-clip-text text-center text-4xl/12 text-transparent">
           Sundays are the primary day that Columbia View Kids gather to play,
           learn from Scripture, pray, and be in community. Our classrooms are{' '}
@@ -31,10 +32,33 @@ export default function NextGen() {
           <span className="font-bold">fun</span>, and{' '}
           <span className="font-bold">Jesus-centered</span> environments.
         </h2>
-        <div className="grid grid-cols-3 grid-rows-1 gap-20">
-          <div className="h-10 bg-black"></div>
-          <div className="h-10 bg-black"></div>
-          <div className="h-10 bg-black"></div>
+        <div className="grid grid-cols-3 grid-rows-1 gap-15">
+          <KeywordCard
+            keyword="Safe"
+            text={`We comply with the state of Oregon's safety standards for childcare including cleanliness, as well as, background checks, and proper training for adult volunteers. You can rest assured that your kids are in a safe place each week!`}
+            imageProps={{
+              src: immigrantSuccess,
+              alt: '',
+            }}
+          />
+          <KeywordCard
+            keyword="Fun"
+            text={`Kids are built for fun and we want to provide an environment where they can be who they were created to be! Each week our schedule is carefully crafted to create an engaging experience of learning that each kid will enjoy and want more of!`}
+            imageProps={{
+              src: immigrantSuccess,
+              alt: '',
+            }}
+          />
+          <KeywordCard
+            keyword="All About Jesus"
+            text={`Our desire is to point kids to the hope of the world: Jesus. We want every kid to hear about Jesus every week so they can know him by his love, forgiveness, and sacrifice for them. Jesus loves your kids and we want them to know that love!`}
+            imageProps={{
+              src: immigrantSuccess,
+              alt: '',
+            }}
+          />
+        </div>
+      </section>
         </div>
       </section>
     </main>
---
+++ components/KeywordCard.tsx
@@ -0,0 +1,35 @@
+import type { ImageProps } from 'next/image'
+
+import Image from 'next/image'
+
+export type KeywordCardProps = {
+  keyword: string
+  text: string
+  imageProps: ImageProps
+}
+
+export default function KeywordCard({
+  keyword,
+  text,
+  imageProps,
+}: KeywordCardProps) {
+  return (
+    <div className="h-full min-h-125 w-full overflow-hidden rounded-2xl bg-white">
+      <div className="relative flex aspect-4/3 items-center justify-center">
+        <Image
+          className="absolute top-0 h-full object-cover opacity-80"
+          {...imageProps}
+        />
+        <p
+          className="font-display absolute text-center text-6xl font-light text-white uppercase"
+          style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}
+        >
+          {keyword}
+        </p>
+      </div>
+      <div className="p-12">
+        <p className="text-center text-base">{text}</p>
+      </div>
+    </div>
+  )
+}
